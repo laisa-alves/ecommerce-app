0a103089736dbf871ce0750f116fd7dbd802c004
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2023-09-19 18:32:24 -0300

    Banner carousel

--- dani_tech/lib/constants.dart
+++ dani_tech/lib/constants.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0xFF5063BF);
+const kPrimaryColor = Color(0xFF1041A2);
 const kPrimaryLightColor = Color(0xFFB9C3FF);
 const kPrimaryGradientColor = LinearGradient(
     begin: Alignment.topLeft,
     end: Alignment.bottomRight,
     colors: [Color(0xFF8EDFEB), Color(0xFF5063BF)]);
-const kSecondaryColor = Color(0xFF8EDFEB);
-const kTextColor = Color(0xFF1E1E1E);
+const kSecondaryColor = Color(0xFFCCC3F9);
+const kTextColor = Color(0xFF060216);
 
 const kAnimationDuration = Duration(milliseconds: 200);
--- dani_tech/lib/screens/home_screen/components/carousel.dart
+++ dani_tech/lib/screens/home_screen/components/carousel.dart
@@ -3,8 +3,19 @@ import 'package:dani_tech/size_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class CarouselHome extends StatelessWidget {
-  final List<String> offersImages = [];
+class CarouselHome extends StatefulWidget {
+  @override
+  State<CarouselHome> createState() => _CarouselHomeState();
+}
+
+class _CarouselHomeState extends State<CarouselHome> {
+  int _currentImage = 0;
+
+  final List<String> offersImages = [
+    'images/home_banner/banner_tv.png',
+    'images/home_banner/banner_setup.png',
+    'images/home_banner/banner_celular.png'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -12,11 +23,38 @@ class CarouselHome extends StatelessWidget {
       child: Column(
         children: [
           SizedBox(
-            width: getProportionateScreenWidth(350),
-            height: getProportionateScreenHeight(171),
-          )
+            height: getProportionateScreenHeight(20),
+          ),
+          SizedBox(
+              width: getProportionateScreenWidth(350),
+              height: getProportionateScreenHeight(171),
+              child: PageView.builder(
+                itemCount: offersImages.length,
+                onPageChanged: (value) {
+                  setState(() {
+                    _currentImage = value;
+                  });
+                },
+                itemBuilder: (context, index) =>
+                    Image.asset(offersImages[index]),
+              ))
         ],
       ),
     );
+  }
+}
+
+// Dots de navegação
+class DotBuilder extends StatelessWidget {
+  const DotBuilder({
+    super.key,
+    required this.isActive,
+  });
+
+  final bool isActive;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
